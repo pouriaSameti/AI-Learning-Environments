401cd16300d5542d136b0017d0a9353e2e817a98
Pouria Sameti <pouria.sameti80@gmail.com>
2025-03-02 01:07:13 +0330

    MDP -> AngryBirds class initialization.

--- MDP/environment.py
+++ MDP/environment.py
@@ -70,3 +70,54 @@ class PygameInit:
         clock = pygame.time.Clock()
 
         return screen, clock
+
+
+#######################################################
+#                DONT CHANGE THIS PART                #
+#######################################################
+class AngryBirds:
+    def __init__(self):
+        self.__grid_size = 8
+        self.__tile_size = 100
+        self.__num_pigs = PIGS
+        self.__num_queens = QUEENS
+        self.__num_rocks = ROCKS
+        self.__probability_dict = self.__generate_probability_dict()
+        self.__base_grid = self.__generate_grid()
+        self.__agent_pos = (0, 0)
+
+        self.grid = copy.deepcopy(self.__base_grid)
+        self.reward = 0
+        self.done = False
+        self.reward_map = self.reward_function()
+        self.transition_table = self.__calculate_transition_model(self.__grid_size, self.__probability_dict,
+                                                                  self.reward_map)
+
+        self.__agent_image = pygame.image.load("Env/icons/angry-birds.png")
+        self.__agent_image = pygame.transform.scale(self.__agent_image, (self.__tile_size, self.__tile_size))
+
+        self.__pig_image = pygame.image.load('Env/icons/pigs.png')
+        self.__pig_image = pygame.transform.scale(self.__pig_image, (self.__tile_size, self.__tile_size))
+        self.__pig_with_background = pygame.Surface((self.__tile_size, self.__tile_size))
+        self.__pig_with_background.fill((135, 206, 235))
+        self.__pig_with_background.blit(self.__pig_image, (0, 0))
+
+        self.__egg_image = pygame.image.load('Env/icons/eggs.png')
+        self.__egg_image = pygame.transform.scale(self.__egg_image, (self.__tile_size, self.__tile_size))
+        self.__egg_with_background = pygame.Surface((self.__tile_size, self.__tile_size))
+        self.__egg_with_background.fill((135, 206, 235))
+        self.__egg_with_background.blit(self.__egg_image, (0, 0))
+
+        self.__queen_image = pygame.image.load('Env/icons/queen.png')
+        self.__queen_image = pygame.transform.scale(self.__queen_image, (self.__tile_size, self.__tile_size))
+        self.__queen_with_background = pygame.Surface((self.__tile_size, self.__tile_size))
+        self.__queen_with_background.fill((135, 206, 235))
+        self.__queen_with_background.blit(self.__queen_image, (0, 0))
+
+        self.__rock_image = pygame.image.load('Env/icons/rocks.png')
+        self.__rock_image = pygame.transform.scale(self.__rock_image, (self.__tile_size, self.__tile_size))
+        self.__rock_with_background = pygame.Surface((self.__tile_size, self.__tile_size))
+        self.__rock_with_background.fill((135, 206, 235))
+        self.__rock_with_background.blit(self.__rock_image, (0, 0))
+
+        
